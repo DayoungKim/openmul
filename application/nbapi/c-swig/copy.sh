e0af744f63c5cbef21247b39ab738d800da86b23
Kim Dayoung <dayoung.kim@kulcloud.net>
2015-05-13 15:25:26 +0900

    copy.sh TEST-SCRIPT/python path added

--- application/nbapi/c-swig/copy.sh
+++ application/nbapi/c-swig/copy.sh
@@ -2,3 +2,9 @@ cp .libs/_mul_nbapi.so ../py-tornado/app/lib/
 cp .libs/_mul_nbapi.so.0 ../py-tornado/app/lib/
 cp .libs/_mul_nbapi.so.0.0.0 ../py-tornado/app/lib/
 cp mul_nbapi.py ../py-tornado/app/lib/
+
+cp .libs/_mul_nbapi.so ../../../TEST-SCRIPTS/python/
+cp .libs/_mul_nbapi.so.0 ../../../TEST-SCRIPTS/python/
+cp .libs/_mul_nbapi.so.0.0.0 ../../../TEST-SCRIPTS/python/
+cp mul_nbapi.py ../../../TEST-SCRIPTS/python/
+
